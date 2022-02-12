7b175f4e05ba742965b8d5d3af8a17cb3fc9ce7e
Somanath Havinal <somusp75@gmail.com>
2022-02-12 23:11:24 +0530

    Add project files.

---
+++ AddressBookSystem_ADO.NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookSystem_ADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
